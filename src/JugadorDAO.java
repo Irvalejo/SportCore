3fc75abd4dcd5edeb3dff8f29bf7a69c98bc28e7
senna <a223212488@unison.mx>
2025-06-25 01:06:19 -0700

    4ta correccion de error

--- src/JugadorDAO.java
+++ src/JugadorDAO.java
@@ -129,7 +129,7 @@ public class JugadorDAO {
         return jugadores;
     }
 
-//pita
+//pite
     public void actualizarJugador(Jugador jugador) {
         String sql = "UPDATE jugador SET Nombre = ?, Apellido = ?, FechaNacimiento = ?, Correo = ?, Foto = ?, EquipoID = ? WHERE ID = ?";
         try (Connection conn = new DBConnection().getConnection();
